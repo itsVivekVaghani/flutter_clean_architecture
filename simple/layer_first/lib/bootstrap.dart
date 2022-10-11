669c76fd233aba85c359941ab94fc446d53eb588
vaghanivivek64 <vaghanivivek555@gmail.com>
2022-10-11 09:40:05 +0530

    Update bootstrap.dart

--- simple/layer_first/lib/bootstrap.dart
+++ simple/layer_first/lib/bootstrap.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 import 'package:layer_first/app/app.dart';
@@ -14,7 +13,7 @@ void bootstrap() {
   Bloc.observer = AppBlocObserver();
 
   runZonedGuarded(
-        () => runApp(const App()),
-        (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+    () => runApp(const App()),
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
